cc71bbad86b92e5bb9a8f2225963228c0694f23e
oskari6 <oskarisulkakoski@gmail.com>
2024-09-03 20:17:14 +0300

    video, formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,8 +27,8 @@ export default function Home() {
                             xmlns="http://www.w3.org/2000/svg"
                           >
                             <path
-                              fill-rule="evenodd"
-                              clip-rule="evenodd"
+                              fillRule="evenodd"
+                              clipRule="evenodd"
                               d="M8 16L3.54223 12.3383C1.93278 11.0162 1 9.04287 1 6.96005C1 3.11612 4.15607 0 8 0C11.8439 0 15 3.11612 15 6.96005C15 9.04287 14.0672 11.0162 12.4578 12.3383L8 16ZM3 6H5C6.10457 6 7 6.89543 7 8V9L3 7.5V6ZM11 6C9.89543 6 9 6.89543 9 8V9L13 7.5V6H11Z"
                               fill="#000000"
                             />
@@ -83,8 +83,8 @@ export default function Home() {
                         xmlns="http://www.w3.org/2000/svg"
                       >
                         <path
-                          fill-rule="evenodd"
-                          clip-rule="evenodd"
+                          fillRule="evenodd"
+                          clipRule="evenodd"
                           d="M12 20 L7.54223 16.3383 C5.93278 15.0162 5 13.04287 5 10.96005 C5 7.11612 8.15607 4 12 4 C15.8439 4 19 7.11612 19 10.96005 C19 13.04287 18.0672 15.0162 16.4578 16.3383 L12 20 Z M7 10 H9 C10.10457 10 11 10.89543 11 12 V13 L7 11.5 V10 Z M15 10 C13.89543 10 13 10.89543 13 12 V13 L17 11.5 V10 H15 Z
 "
                           fill="#000000"
@@ -136,7 +136,7 @@ export default function Home() {
                         >
                           <path
                             stroke="currentColor"
-                            stroke-width="1.5"
+                            strokeWidth="1.5"
                             d="M3.75 21v-3a3.75 3.75 0 013.75-3.75h9A3.75 3.75 0 0120.25 18v3M12 3.75a3.75 3.75 0 100 7.5 3.75 3.75 0 000-7.5z"
                           ></path>
                         </svg>
@@ -154,7 +154,7 @@ export default function Home() {
                           >
                             <path
                               stroke="currentColor"
-                              stroke-width="1.5"
+                              strokeWidth="1.5"
                               d="M8.25 8.25V6a2.25 2.25 0 012.25-2.25h3a2.25 2.25 0 110 4.5H3.75v8.25a3.75 3.75 0 003.75 3.75h9a3.75 3.75 0 003.75-3.75V8.25H17.5"
                             ></path>
                           </svg>
@@ -200,7 +200,16 @@ export default function Home() {
                         <div className="media-wrapper-6">
                           <div className="rvp-video-root">
                             <div className="video-container">
-                              <video></video>
+                              <video
+                                playsInline
+                                loop
+                                id="video-1"
+                                role="application"
+                                preload="auto"
+                                muted
+                                autoPlay
+                                src="/sample-video.mp4"
+                              ></video>
                               <div className="video-bg-image"></div>
                               <div className="video-element">
                                 <div className="video-element-2"></div>
@@ -220,8 +229,12 @@ export default function Home() {
                               </h1>
                               <p>Get set for school</p>
                               <div className="cta-container">
-                                <a>Shop Kids</a>
-                                <a>Shop All</a>
+                                <a>
+                                  Shop Kids<span className="ripple"></span>
+                                </a>
+                                <a>
+                                  Shop All<span className="ripple"></span>
+                                </a>
                               </div>
                             </div>
                           </div>
@@ -236,88 +249,92 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <footer className="container-footer">
-        <div className="container-20"></div>
-        <div className="container-6">
-          <div className="container-7">
-            <a>
-              <p className="footer-header">PRODUCTS</p>
-            </a>
-            <p className="style-1">Shoes</p>
-            <p className="style-1">Clothing</p>
-            <p className="style-1">Accessories</p>
-            <p className="style-1">New Arrivals</p>
-            <p className="style-1">Release Dates</p>
-            <p className="style-1">Outlet</p>
+      <div>
+        <footer className="container-footer">
+          <div className="container-20"></div>
+          <div className="container-6">
+            <div className="container-7">
+              <a>
+                <p className="footer-header">PRODUCTS</p>
+              </a>
+              <p className="style-1">Shoes</p>
+              <p className="style-1">Clothing</p>
+              <p className="style-1">Accessories</p>
+              <p className="style-1">New Arrivals</p>
+              <p className="style-1">Release Dates</p>
+              <p className="style-1">Outlet</p>
+            </div>
+            <div className="container-7">
+              <a>
+                <p className="footer-header">SPORTS</p>
+              </a>
+              <p className="style-1">Football</p>
+              <p className="style-1">Basketball</p>
+              <p className="style-1">Golf</p>
+              <p className="style-1">Gym & Training</p>
+              <p className="style-1">Outdoor</p>
+              <p className="style-1">Rugby</p>
+              <p className="style-1">Running</p>
+              <p className="style-1">Tennis</p>
+            </div>
+            <div className="container-7">
+              <a>
+                <p className="footer-header">COLLECTIONS</p>
+              </a>
+              <p className="style-1">Originals</p>
+              <p className="style-1">Spezial</p>
+              <p className="style-1">SL72</p>
+              <p className="style-1">Campus</p>
+              <p className="style-1">Adizero</p>
+              <p className="style-1">Samba</p>
+              <p className="style-1">Gazelle</p>
+              <p className="style-1">adicolor</p>
+            </div>
+            <div className="container-8-wrapper">
+              <div className="container-8">
+                <button>
+                  <span>
+                    <svg
+                      aria-hidden="true"
+                      className="css-npy3on"
+                      focusable="false"
+                      viewBox="0 0 24 24"
+                      role="img"
+                      width="24px"
+                      height="24px"
+                      fill="none"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeMiterlimit="10"
+                        strokeWidth="1.5"
+                        d="M21.75 12A9.75 9.75 0 0112 21.75M21.75 12A9.75 9.75 0 0012 2.25M21.75 12c0 2.071-4.365 3.75-9.75 3.75S2.25 14.071 2.25 12m19.5 0c0-2.071-4.365-3.75-9.75-3.75S2.25 9.929 2.25 12M12 21.75A9.75 9.75 0 012.25 12M12 21.75c2.9 0 5.25-4.365 5.25-9.75S14.9 2.25 12 2.25m0 19.5c-2.9 0-5.25-4.365-5.25-9.75S9.1 2.25 12 2.25M2.25 12A9.75 9.75 0 0112 2.25"
+                      ></path>
+                    </svg>
+                  </span>
+                  Finland
+                </button>
+              </div>
+            </div>
           </div>
-          <div className="container-7">
-            <a>
-              <p className="footer-header">SPORTS</p>
-            </a>
-            <p className="style-1">Football</p>
-            <p className="style-1">Basketball</p>
-            <p className="style-1">Golf</p>
-            <p className="style-1">Gym & Training</p>
-            <p className="style-1">Outdoor</p>
-            <p className="style-1">Rugby</p>
-            <p className="style-1">Running</p>
-            <p className="style-1">Tennis</p>
-          </div>
-          <div className="container-7">
-            <a>
-              <p className="footer-header">COLLECTIONS</p>
-            </a>
-            <p className="style-1">Originals</p>
-            <p className="style-1">Spezial</p>
-            <p className="style-1">SL72</p>
-            <p className="style-1">Campus</p>
-            <p className="style-1">Adizero</p>
-            <p className="style-1">Samba</p>
-            <p className="style-1">Gazelle</p>
-            <p className="style-1">adicolor</p>
-          </div>
-          <div className="container-8">
-            <button>
+          <div className="footer-spacer"></div>
+          <div className="container-19">
+            <ul className="footer-list">
+              <p>Cookie Settings</p>
+              <p>Data Settings</p>
+              <p>Cookies</p>
+              <p>Privace Notice</p>
+              <p>Terms and Conditions</p>
               <span>
-                <svg
-                  aria-hidden="true"
-                  className="css-npy3on"
-                  focusable="false"
-                  viewBox="0 0 24 24"
-                  role="img"
-                  width="24px"
-                  height="24px"
-                  fill="none"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-miterlimit="10"
-                    stroke-width="1.5"
-                    d="M21.75 12A9.75 9.75 0 0112 21.75M21.75 12A9.75 9.75 0 0012 2.25M21.75 12c0 2.071-4.365 3.75-9.75 3.75S2.25 14.071 2.25 12m19.5 0c0-2.071-4.365-3.75-9.75-3.75S2.25 9.929 2.25 12M12 21.75A9.75 9.75 0 012.25 12M12 21.75c2.9 0 5.25-4.365 5.25-9.75S14.9 2.25 12 2.25m0 19.5c-2.9 0-5.25-4.365-5.25-9.75S9.1 2.25 12 2.25M2.25 12A9.75 9.75 0 0112 2.25"
-                  ></path>
-                </svg>
+                Discount coupons can be used on a wide range of your favorite
+                adidas items; certain exclusions and sales restrictions apply.
+                Visit https://www.adidas.fi/selected_items | adidas.fi for
+                eligible items.
               </span>
-              Finland
-            </button>
+            </ul>
           </div>
-        </div>
-        <div className="footer-spacer"></div>
-        <div className="container-19">
-          <ul className="footer-list">
-            <p>Cookie Settings</p>
-            <p>Data Settings</p>
-            <p>Cookies</p>
-            <p>Privace Notice</p>
-            <p>Terms and Conditions</p>
-            <span>
-              Discount coupons can be used on a wide range of your favorite
-              adidas items; certain exclusions and sales restrictions apply.
-              Visit https://www.adidas.fi/selected_items | adidas.fi for
-              eligible items.
-            </span>
-          </ul>
-        </div>
-      </footer>
+        </footer>
+      </div>
     </>
   );
 }
